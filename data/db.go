364e206d6dd57df35029d847cf6e9cebfe6c4e03
Anirudh Konidala <kidskodingclub@gmail.com>
2025-06-08 20:42:53 -0500

    created join table script to join types with pokemon

--- data/db.go
+++ data/db.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"os"
+
 	"github.com/jackc/pgx/v5"
 	"github.com/lpernett/godotenv"
 )
@@ -18,7 +19,7 @@ func Connect() *pgx.Conn {
 	return conn
 }
 
-func InsertPokemon(conn *pgx.Conn, pokemon BaseStats) {
+func InsertPokemon(conn *pgx.Conn, pokemon BaseStats) int {
 	query := `INSERT INTO base_stats (id, name, generation, hp, attack, defense, sp_attack, sp_defense, speed) 
 		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
 		ON CONFLICT (id) DO NOTHING`
@@ -40,29 +41,56 @@ func InsertPokemon(conn *pgx.Conn, pokemon BaseStats) {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	return pokemon.ID
 }
 
 func InsertPokemonType(conn *pgx.Conn, pokemonTypes [][]string) {
 	var count int
 	err := conn.QueryRow(context.Background(), "SELECT COUNT(*) FROM types").Scan(&count)
 	if err != nil {
-		log.Fatalf("Failed to count rows in types table: %v", err)
+		log.Fatalf("failed to count rows in types table: %v", err)
 	}
 	if count > 0 {
-		log.Println("Types table already populated. Skipping insert.")
+		log.Println("types table already populated. skipping insert.")
 		return
 	}
 
-	query := `INSERT INTO types (primary_type, secondary_type) VALUES ($1, $2)`	
+	query := `INSERT INTO types (type) VALUES ($1)`	
 
 	for i := range pokemonTypes {
 		if len(pokemonTypes[i]) == 1 {
 			conn.Exec(context.Background(), query, pokemonTypes[i][0])
 		} else {
-			_, err := conn.Exec(context.Background(), query, pokemonTypes[i][0], pokemonTypes[i][1])
-			if err != nil {
-				log.Fatal(err)
+			for j := range pokemonTypes[i] {
+				_, err := conn.Exec(context.Background(), query, pokemonTypes[i][j])
+				if err != nil {
+					log.Fatal(err)	
+				}
 			}
+		}
+	}
+}
+
+func InsertPokemonTypeLink(conn *pgx.Conn, pokemonID int, typeNames []string) {
+	joinTableQuery := `INSERT INTO pokemon_types_link (pokemon_id, type_id)
+		VALUES ($1, $2)
+		ON CONFLICT DO NOTHING`
+
+	for _, typeName := range typeNames {
+		var typeID int
+		err := conn.QueryRow(
+			context.Background(), 
+			"SELECT id FROM types WHERE type=$1",
+			typeName,
+		).Scan(&typeID)
+		if err != nil {
+			log.Fatalf("failed to get type ID for %s: %v", typeName, err)
+		}
+
+		_, err = conn.Exec(context.Background(), joinTableQuery, pokemonID, typeID)
+		if err != nil {
+			log.Fatalf("failed to insert pokemon_types link: %v", err)
 		}
 	}
 }
--- data/main.go
+++ data/main.go
@@ -4,14 +4,14 @@ import "context"
 
 func main() {
 	conn := Connect()
+	defer conn.Close(context.Background())
 
 	stats := GetPokemonBaseStats(1)
 	types := GetPokemonTypes(1)
 
 	for i := range stats {
-		InsertPokemon(conn, stats[i])
+		pokemonID := InsertPokemon(conn, stats[i])
+		InsertPokemonType(conn, types)
+		InsertPokemonTypeLink(conn, pokemonID, types[i])
 	}
-	InsertPokemonType(conn, types)
-
-	defer conn.Close(context.Background())
 }
